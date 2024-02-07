8d8c19443c7276e02b234d8f13508c331456894e
Serref Jaguraga <serrefjaguraga@serrefs-mbp.mynetworksettings.com>
2024-02-07 10:04:40 -0500

    created skills section

--- components/projects.tsx
+++ components/projects.tsx
@@ -9,7 +9,7 @@ import { useSectionInView } from "@/lib/hooks";
 const Projects = () => {
   const { ref } = useSectionInView("Projects", 0.5);
   return (
-    <section ref={ref} className="scroll-mt-28" id="projects">
+    <section ref={ref} className="scroll-mt-28 mb-28" id="projects">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
