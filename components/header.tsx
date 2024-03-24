5cd0e9eec689cf3faa8a0d160d72196c290287a4
Serref Jaguraga <serrefjaguraga@Savannahs-Air.localdomain>
2024-03-24 07:43:56 -0400

    added some dark mode features to some components

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
@@ -29,8 +29,11 @@ const Header = () => {
             >
               <Link
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition ",
-                  { "text-gray-950": activeSection === link.name }
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  {
+                    "text-gray-950 dark:text-gray-200":
+                      activeSection === link.name,
+                  }
                 )}
                 href={link.hash}
                 onClick={() => {
@@ -44,7 +47,7 @@ const Header = () => {
                   <motion.span
                     layoutId="activeSection"
                     transition={{ type: "spring", stiffness: 380, damping: 30 }}
-                    className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
                   ></motion.span>
                 )}
               </Link>
--- components/intro.tsx
+++ components/intro.tsx
@@ -87,7 +87,7 @@ const Intro = () => {
         <a
           href="/CV.pdf"
           download
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10 dark:bg-white/10"
         >
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
@@ -96,7 +96,7 @@ const Intro = () => {
         <a
           href="https://www.linkedin.com/in/serref-jaguraga-2827041b6/"
           target="_blank"
-          className="bg-white p-4 text-[#0b66c2] flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-[#0b54c2] active:scale-105 transition border border-black/10"
+          className="bg-white p-4 text-[#0b66c2] flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-[#0b54c2] active:scale-105 transition border border-black/10 dark:bg-white/10"
         >
           <BsLinkedin />
         </a>
@@ -104,7 +104,7 @@ const Intro = () => {
         <a
           href="https://github.com/Sjaguraga"
           target="_blank"
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10 dark:bg-white/10 dark:text-white/60"
         >
           <FaGithub />
         </a>
